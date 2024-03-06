1713195db6bc0bcd7b9a95324b44a6f3b0fc6da3
avkhlevchuk <avkhlevchuk>
2024-03-06 14:17:29 +0000

    delete label

---
+++ Uddem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uddem
+//
+//  Created by Artem Khlevchuk on 06.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+           
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uddem/UddemApp.swift
@@ -0,0 +1,17 @@
+//
+//  UddemApp.swift
+//  Uddem
+//
+//  Created by Artem Khlevchuk on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct UddemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
